56bd8c052802a9090906a4d9daab746b8bdc2acf
Arjan <357noob@gmail.com>
2022-01-30 19:44:04 +0000

    ppu

--- NES Emulator/ppu.cpp
+++ NES Emulator/ppu.cpp
@@ -2,73 +2,74 @@
 #include <cstdint>
 
 ppu::ppu() {
-	palScreen[0x00] = olc::Pixel(84, 84, 84);
-	palScreen[0x01] = olc::Pixel(0, 30, 116);
-	palScreen[0x02] = olc::Pixel(8, 16, 144);
-	palScreen[0x03] = olc::Pixel(48, 0, 136);
-	palScreen[0x04] = olc::Pixel(68, 0, 100);
-	palScreen[0x05] = olc::Pixel(92, 0, 48);
-	palScreen[0x06] = olc::Pixel(84, 4, 0);
-	palScreen[0x07] = olc::Pixel(60, 24, 0);
-	palScreen[0x08] = olc::Pixel(32, 42, 0);
-	palScreen[0x09] = olc::Pixel(8, 58, 0);
-	palScreen[0x0A] = olc::Pixel(0, 64, 0);
-	palScreen[0x0B] = olc::Pixel(0, 60, 0);
-	palScreen[0x0C] = olc::Pixel(0, 50, 60);
-	palScreen[0x0D] = olc::Pixel(0, 0, 0);
-	palScreen[0x0E] = olc::Pixel(0, 0, 0);
-	palScreen[0x0F] = olc::Pixel(0, 0, 0);
+	// array of every colour the nes is capable of displaying
+	palettes[0x00] = olc::Pixel(84, 84, 84);
+	palettes[0x01] = olc::Pixel(0, 30, 116);
+	palettes[0x02] = olc::Pixel(8, 16, 144);
+	palettes[0x03] = olc::Pixel(48, 0, 136);
+	palettes[0x04] = olc::Pixel(68, 0, 100);
+	palettes[0x05] = olc::Pixel(92, 0, 48);
+	palettes[0x06] = olc::Pixel(84, 4, 0);
+	palettes[0x07] = olc::Pixel(60, 24, 0);
+	palettes[0x08] = olc::Pixel(32, 42, 0);
+	palettes[0x09] = olc::Pixel(8, 58, 0);
+	palettes[0x0A] = olc::Pixel(0, 64, 0);
+	palettes[0x0B] = olc::Pixel(0, 60, 0);
+	palettes[0x0C] = olc::Pixel(0, 50, 60);
+	palettes[0x0D] = olc::Pixel(0, 0, 0);
+	palettes[0x0E] = olc::Pixel(0, 0, 0);
+	palettes[0x0F] = olc::Pixel(0, 0, 0);
 
-	palScreen[0x10] = olc::Pixel(152, 150, 152);
-	palScreen[0x11] = olc::Pixel(8, 76, 196);
-	palScreen[0x12] = olc::Pixel(48, 50, 236);
-	palScreen[0x13] = olc::Pixel(92, 30, 228);
-	palScreen[0x14] = olc::Pixel(136, 20, 176);
-	palScreen[0x15] = olc::Pixel(160, 20, 100);
-	palScreen[0x16] = olc::Pixel(152, 34, 32);
-	palScreen[0x17] = olc::Pixel(120, 60, 0);
-	palScreen[0x18] = olc::Pixel(84, 90, 0);
-	palScreen[0x19] = olc::Pixel(40, 114, 0);
-	palScreen[0x1A] = olc::Pixel(8, 124, 0);
-	palScreen[0x1B] = olc::Pixel(0, 118, 40);
-	palScreen[0x1C] = olc::Pixel(0, 102, 120);
-	palScreen[0x1D] = olc::Pixel(0, 0, 0);
-	palScreen[0x1E] = olc::Pixel(0, 0, 0);
-	palScreen[0x1F] = olc::Pixel(0, 0, 0);
+	palettes[0x10] = olc::Pixel(152, 150, 152);
+	palettes[0x11] = olc::Pixel(8, 76, 196);
+	palettes[0x12] = olc::Pixel(48, 50, 236);
+	palettes[0x13] = olc::Pixel(92, 30, 228);
+	palettes[0x14] = olc::Pixel(136, 20, 176);
+	palettes[0x15] = olc::Pixel(160, 20, 100);
+	palettes[0x16] = olc::Pixel(152, 34, 32);
+	palettes[0x17] = olc::Pixel(120, 60, 0);
+	palettes[0x18] = olc::Pixel(84, 90, 0);
+	palettes[0x19] = olc::Pixel(40, 114, 0);
+	palettes[0x1A] = olc::Pixel(8, 124, 0);
+	palettes[0x1B] = olc::Pixel(0, 118, 40);
+	palettes[0x1C] = olc::Pixel(0, 102, 120);
+	palettes[0x1D] = olc::Pixel(0, 0, 0);
+	palettes[0x1E] = olc::Pixel(0, 0, 0);
+	palettes[0x1F] = olc::Pixel(0, 0, 0);
 
-	palScreen[0x20] = olc::Pixel(236, 238, 236);
-	palScreen[0x21] = olc::Pixel(76, 154, 236);
-	palScreen[0x22] = olc::Pixel(120, 124, 236);
-	palScreen[0x23] = olc::Pixel(176, 98, 236);
-	palScreen[0x24] = olc::Pixel(228, 84, 236);
-	palScreen[0x25] = olc::Pixel(236, 88, 180);
-	palScreen[0x26] = olc::Pixel(236, 106, 100);
-	palScreen[0x27] = olc::Pixel(212, 136, 32);
-	palScreen[0x28] = olc::Pixel(160, 170, 0);
-	palScreen[0x29] = olc::Pixel(116, 196, 0);
-	palScreen[0x2A] = olc::Pixel(76, 208, 32);
-	palScreen[0x2B] = olc::Pixel(56, 204, 108);
-	palScreen[0x2C] = olc::Pixel(56, 180, 204);
-	palScreen[0x2D] = olc::Pixel(60, 60, 60);
-	palScreen[0x2E] = olc::Pixel(0, 0, 0);
-	palScreen[0x2F] = olc::Pixel(0, 0, 0);
+	palettes[0x20] = olc::Pixel(236, 238, 236);
+	palettes[0x21] = olc::Pixel(76, 154, 236);
+	palettes[0x22] = olc::Pixel(120, 124, 236);
+	palettes[0x23] = olc::Pixel(176, 98, 236);
+	palettes[0x24] = olc::Pixel(228, 84, 236);
+	palettes[0x25] = olc::Pixel(236, 88, 180);
+	palettes[0x26] = olc::Pixel(236, 106, 100);
+	palettes[0x27] = olc::Pixel(212, 136, 32);
+	palettes[0x28] = olc::Pixel(160, 170, 0);
+	palettes[0x29] = olc::Pixel(116, 196, 0);
+	palettes[0x2A] = olc::Pixel(76, 208, 32);
+	palettes[0x2B] = olc::Pixel(56, 204, 108);
+	palettes[0x2C] = olc::Pixel(56, 180, 204);
+	palettes[0x2D] = olc::Pixel(60, 60, 60);
+	palettes[0x2E] = olc::Pixel(0, 0, 0);
+	palettes[0x2F] = olc::Pixel(0, 0, 0);
 
-	palScreen[0x30] = olc::Pixel(236, 238, 236);
-	palScreen[0x31] = olc::Pixel(168, 204, 236);
-	palScreen[0x32] = olc::Pixel(188, 188, 236);
-	palScreen[0x33] = olc::Pixel(212, 178, 236);
-	palScreen[0x34] = olc::Pixel(236, 174, 236);
-	palScreen[0x35] = olc::Pixel(236, 174, 212);
-	palScreen[0x36] = olc::Pixel(236, 180, 176);
-	palScreen[0x37] = olc::Pixel(228, 196, 144);
-	palScreen[0x38] = olc::Pixel(204, 210, 120);
-	palScreen[0x39] = olc::Pixel(180, 222, 120);
-	palScreen[0x3A] = olc::Pixel(168, 226, 144);
-	palScreen[0x3B] = olc::Pixel(152, 226, 180);
-	palScreen[0x3C] = olc::Pixel(160, 214, 228);
-	palScreen[0x3D] = olc::Pixel(160, 162, 160);
-	palScreen[0x3E] = olc::Pixel(0, 0, 0);
-	palScreen[0x3F] = olc::Pixel(0, 0, 0);
+	palettes[0x30] = olc::Pixel(236, 238, 236);
+	palettes[0x31] = olc::Pixel(168, 204, 236);
+	palettes[0x32] = olc::Pixel(188, 188, 236);
+	palettes[0x33] = olc::Pixel(212, 178, 236);
+	palettes[0x34] = olc::Pixel(236, 174, 236);
+	palettes[0x35] = olc::Pixel(236, 174, 212);
+	palettes[0x36] = olc::Pixel(236, 180, 176);
+	palettes[0x37] = olc::Pixel(228, 196, 144);
+	palettes[0x38] = olc::Pixel(204, 210, 120);
+	palettes[0x39] = olc::Pixel(180, 222, 120);
+	palettes[0x3A] = olc::Pixel(168, 226, 144);
+	palettes[0x3B] = olc::Pixel(152, 226, 180);
+	palettes[0x3C] = olc::Pixel(160, 214, 228);
+	palettes[0x3D] = olc::Pixel(160, 162, 160);
+	palettes[0x3E] = olc::Pixel(0, 0, 0);
+	palettes[0x3F] = olc::Pixel(0, 0, 0);
 }
 ppu::~ppu() {
 
@@ -83,8 +84,34 @@ olc::Sprite& ppu::GetNameTable(uint8_t i)
 	return sprNameTable[i];
 }
 
-olc::Sprite& ppu::GetPatternTable(uint8_t i)
+olc::Pixel& ppu::GetColourFromPaletteRam(uint8_t palette, uint8_t pixel) {
+	return palettes[ppuread((0x3F00 + (palette * 4) + pixel) & 0x3F, false)];
+}
+
+olc::Sprite& ppu::GetPatternTable(uint8_t i, uint8_t palette)
 {
+	// pattern table is 16x16
+	for (uint16_t x = 0; x < 16; x++) {
+		for (uint16_t y = 0; y < 16; y++) {
+			// a single tile is 16 bytes and there are 16 of them in a single row 
+			uint16_t offset = y * 256 + x * 16;
+			// single tile is 8x8
+			for (uint16_t row = 0; row < 8; row++) {
+				// the nes pattern tables used 2 bits per pixel, so 4 colours were available. rather than storing each colour code on 
+				// one single tile, they were split up into least significant bit and most significant bit, so they had to be combined to get a whole pixel
+				uint8_t tile_lsb = ppuread(i * 0x1000 + offset + row, false); 
+				uint8_t tile_msb = ppuread(i * 0x1000 + offset + row + 8, false);
+				for (uint16_t column = 0; column < 8; column++) {
+					// will only preserve the least significant bit, so a value between 0 and 3 will be returned
+					uint8_t pixel = (tile_lsb & 0x01) + (tile_msb & 0x01);
+					tile_lsb = tile_lsb >> 1;
+					tile_msb = tile_msb >> 1;
+
+					sprPatternTable[i].SetPixel(x * 8 + (7 - column), y * 8 + row, GetColourFromPaletteRam(palette, pixel));
+				}
+			}
+		}
+	}
 	return sprPatternTable[i];
 }
 
@@ -99,7 +126,10 @@ uint8_t ppu::read(uint16_t address, bool readOnly) {
 
 		break;
 	case 0x0002:
-
+		status.vblank = 1;
+		data = (status.reg & 0xE0) | (ppu_data_buffer & 0x1F);
+		status.vblank = 0;
+		address_latch = 0;
 		break;
 	case 0x0003:
 
@@ -111,10 +141,21 @@ uint8_t ppu::read(uint16_t address, bool readOnly) {
 
 		break;
 	case 0x0006:
-
+		if (address_latch == 0) {
+			ppu_address = (ppu_address & 0x00FF) | (data << 8);
+			address_latch = 1;
+		}
+		else {
+			ppu_address = (ppu_address & 0xFF00) | data;
+			address_latch = 0;
+		}
 		break;
 	case 0x0007:
+		data = ppu_data_buffer;
+		ppu_data_buffer = ppuread(ppu_address, false);
 
+		if (ppu_address >= 0x3f00) data = ppu_data_buffer;
+		ppu_address++;
 		break;
 	
 	}
@@ -125,10 +166,10 @@ uint8_t ppu::read(uint16_t address, bool readOnly) {
 void ppu::write(uint16_t address, uint8_t data) {
 	switch (address) {
 	case 0x0000:
-
+		ctrl.reg = data;
 		break;
 	case 0x0001:
-
+		mask.reg = data;
 		break;
 	case 0x0002:
 
@@ -143,10 +184,18 @@ void ppu::write(uint16_t address, uint8_t data) {
 
 		break;
 	case 0x0006:
-
+		if (address_latch == 0) {
+			ppu_address = (ppu_address & 0xFF00) | data;
+			address_latch = 1;
+		}
+		else {
+			ppu_address = (ppu_address & 0xFF00) | data;
+			address_latch = 0;
+		}
 		break;
 	case 0x0007:
-
+		ppuwrite(ppu_address, data);
+		ppu_address++;
 		break;
 
 	}
@@ -158,16 +207,58 @@ uint8_t ppu::ppuread(uint16_t address, bool readOnly) {
 	// cartridge has priority
 	if (cart->ppuRead(address, data)) {
 
-	};
+	}
+	// pattern memory
+	else if (address >= 0x0000 && address <= 0x1FFF) {
+		// chooses whether its the left or right hand side of the pattern table. the most significant bit of the 
+		// ppu address is taken, and the remaining bits are taken to find the offset into that table 
+		data = pattern[(address & 0x1000) >> 12][address & 0x0FFF];
+	}
+	// nametable memory
+	else if (address >= 0x2000 && address <= 0x3EFF) {
+
+	}
+	// palette memory
+	else if (address >= 0x3F00 && address <= 0x3FFF) {
+		// mask first 5 bits 
+		address = address & 0x001F;
+		// mirroring
+		if (address == 0x0010) address = 0x0000;
+		if (address == 0x0014) address = 0x0004;
+		if (address == 0x0018) address = 0x0008;
+		if (address == 0x001C) address = 0x000C;
+		data = palette[address];
+	}
 	return data;
 }
 
 void ppu::ppuwrite(uint16_t address, uint8_t data) {
 	address = address & 0x3FFF;
 	// cartridge has priority
-	if (cart->ppuRead(address, data)) {
+	// cartridge has priority
+	if (cart->ppuWrite(address, data)) {
 
-	};
+	}
+	// pattern memory
+	else if (address >= 0x0000 && address <= 0x1FFF) {
+		// chooses whether its the left or right hand side of the pattern table. the most significant bit of the 
+		// ppu address is taken, and the remaining bits are taken to find the offset into that table 
+		pattern[(address & 0x1000) >> 12][address & 0x0FFF] = data;
+	}
+	// nametable memory
+	else if (address >= 0x2000 && address <= 0x3EFF) {
+
+	}
+	// palette memory
+	else if (address >= 0x3F00 && address <= 0x3FFF) {
+		address = address & 0x001F;
+		// mirroring
+		if (address == 0x0010) address = 0x0000;
+		if (address == 0x0014) address = 0x0004;
+		if (address == 0x0018) address = 0x0008;
+		if (address == 0x001C) address = 0x000C;
+		palette[address] = data;
+	}
 }
 
 void ppu::connectCartridge(const std::shared_ptr<cartridge>& cartridge) {
@@ -176,7 +267,7 @@ void ppu::connectCartridge(const std::shared_ptr<cartridge>& cartridge) {
 
 void ppu::clock() {
 
-	sprScreen.SetPixel(cycle - 1, scanline, palScreen[(rand() % 2) ? 0x3F : 0x30]);
+	sprScreen.SetPixel(cycle - 1, scanline, palettes[(rand() % 2) ? 0x3F : 0x30]);
 	cycle++;
 	if (cycle >= 341) {
 		cycle = 0;
--- NES Emulator/ppu.h
+++ NES Emulator/ppu.h
@@ -22,25 +22,69 @@ public:
 	void connectCartridge(const std::shared_ptr<cartridge>& cartridge);
 	void clock();
 
+	// nametable, pattern and palette memory
+	uint8_t nametable[2][1024];
+	uint8_t pattern[2][256 * 16];
+	uint8_t palette[32];
+	// shares ownership of the cartridge object with bus 
+	std::shared_ptr<cartridge> cart;
 	// ppu contains 8 memory-mapped registers which sit at $2000 to $2007 in the CPU's address space, 2 256-tile pattern tables (which define the shape of the tiles that make up backgrounds and sprites), 
 	// 2 physical 1024 byte nametables (4 logical ones after mirroring), a 64-byte attribute table, OAM memory (contains a display list of up to 64 sprites where each sprite occupies 4 bytes), 64 palette outputs
 	// and a memory map, which addresses a 16kB space from $0000 to $3FFF. 
-	uint8_t nametable[2][1024];
-	uint8_t palettes[64];
-	uint8_t patterntable[2][256 * 16];
-	// shares ownership of the cartridge object with bus 
-	std::shared_ptr<cartridge> cart;
-
-	// for the game engine
-	olc::Sprite& GetScreen();
-	olc::Sprite& GetNameTable(uint8_t i);
-	olc::Sprite& GetPatternTable(uint8_t i);
-	bool frame_complete = false;
-	olc::Pixel  palScreen[0x40];
+	// chr/pattern rom is for sprites, nametable memory is for layout and palette memory is for colours
+	olc::Pixel  palettes[0x40];
 	olc::Sprite sprScreen = olc::Sprite(256, 240);
 	olc::Sprite sprNameTable[2] = { olc::Sprite(256, 240), olc::Sprite(256, 240) };
 	olc::Sprite sprPatternTable[2] = { olc::Sprite(128, 128), olc::Sprite(128, 128) };
+	olc::Sprite& GetScreen();
+	olc::Sprite& GetNameTable(uint8_t i);
+	olc::Sprite& GetPatternTable(uint8_t i, uint8_t palette); // there are two 256-tile sections
+	olc::Pixel& GetColourFromPaletteRam(uint8_t palette, uint8_t pixel);
+	bool frame_complete = false;
 	int16_t scanline = 0;
 	int16_t cycle = 0;
+
+	// ppu control register
+	union PPUCTRL {
+		struct {
+			uint8_t nmiEnable : 1;
+			uint8_t ppuSlave : 1;
+			uint8_t spriteHeight : 1;
+			uint8_t backgroundTileSelect : 1;
+			uint8_t spriteTileSelect : 1;
+			uint8_t incrementMode : 1;
+			uint8_t nametableSelect : 2;
+		};
+		uint8_t reg;
+	} ctrl;
+	// ppu mask register
+	union PPUMASK {
+		struct {
+			uint8_t blueEmphasis : 1;
+			uint8_t greenEmphasis : 1;
+			uint8_t redEmphasis : 1;
+			uint8_t spriteEnable : 1;
+			uint8_t backgroundEnable : 1;
+			uint8_t spriteLeftColumnEnable : 1;
+			uint8_t backgroundLeftColumnEnable : 1;
+			uint8_t greyscale : 1;
+		};
+		uint8_t reg;
+	} mask;
+	// ppu status register
+	union PPUSTATUS {
+		struct {
+			uint8_t vblank : 1;
+			uint8_t sprite0 : 1;
+			uint8_t spriteoverflow : 1;
+			uint8_t unused : 5;
+		};
+
+		uint8_t reg;
+	} status;
+
+	uint8_t address_latch = 0x00;
+	uint8_t ppu_data_buffer = 0x00;
+	uint16_t ppu_address = 0x0000;
 };
 
